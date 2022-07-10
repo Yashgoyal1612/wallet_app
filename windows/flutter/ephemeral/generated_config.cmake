3e4064b98cfc116b4203f85fb5a706b6d129ec46
Yash Goyal <80318226+Yashgoyal1612@users.noreply.github.com>
2022-07-10 20:19:24 +0530

    Changes

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter\\wallet_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=D:\\Flutter\\wallet_app"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter\\wallet_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter\\wallet_app"
+  "FLUTTER_TARGET=D:\\Flutter\\wallet_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter\\wallet_app\\.dart_tool\\package_config.json"
+)
